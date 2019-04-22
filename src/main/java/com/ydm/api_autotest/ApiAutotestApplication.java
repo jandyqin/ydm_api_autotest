b2d740144f11a297ca3065a1f334ca0ecd4854f1
yubei <yubei874721995@163.com>
2019-04-22 12:34:19 +0800

    更新

--- src/main/java/com/ydm/api_autotest/ApiAutotestApplication.java
+++ src/main/java/com/ydm/api_autotest/ApiAutotestApplication.java
@@ -2,8 +2,17 @@ package com.ydm.api_autotest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//启用缓存
+@EnableCaching
+//启用异步
+@EnableAsync
+//定时任务
+@EnableScheduling
 public class ApiAutotestApplication {
 
     public static void main(String[] args) {
